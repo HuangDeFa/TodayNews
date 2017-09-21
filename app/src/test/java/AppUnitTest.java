7da802e9a87b922d53d2acb5b5ad5136eb8d8037
huangdefa <huangdefa@kenpro.local>
2017-09-22 00:43:09 +0800

    一些关于Retrofit结合Rxjava的测试

--- app/src/test/java/AppUnitTest.java
+++ app/src/test/java/AppUnitTest.java
@@ -1,26 +1,34 @@
-import android.os.SystemClock;
-
 import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
 import com.huangdefa.todaynews.Model.ChannelModel;
 import com.huangdefa.todaynews.Model.ChannelViewModel;
 import com.huangdefa.todaynews.Model.ContentModel;
+import com.huangdefa.todaynews.Model.MovieModel;
 import com.huangdefa.todaynews.Net.ApiManager;
 
-import static org.junit.Assert.*;
-
-import org.json.JSONObject;
 import org.junit.Test;
 
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
+import io.reactivex.Observable;
 import io.reactivex.Observer;
 import io.reactivex.annotations.NonNull;
 import io.reactivex.disposables.Disposable;
+import io.reactivex.functions.Function;
+import io.reactivex.schedulers.Schedulers;
+import okhttp3.OkHttpClient;
 import okhttp3.ResponseBody;
+import okhttp3.logging.HttpLoggingInterceptor;
+import retrofit2.Retrofit;
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
+import retrofit2.converter.gson.GsonConverterFactory;
+import retrofit2.http.GET;
+import retrofit2.http.Path;
+
+import static org.junit.Assert.assertEquals;
 
 /**
  * Created by ken.huang on 9/19/2017.
@@ -71,4 +79,96 @@ public class AppUnitTest {
             }
         });
     }
+
+    /**
+     * 使用retrofit
+     */
+    @Test public void retrofitTest(){
+        OkHttpClient okHttpClient=new OkHttpClient.Builder()
+                .connectTimeout(10, TimeUnit.SECONDS)
+                .addInterceptor(new HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY))
+                .build();
+        Retrofit retrofit=new Retrofit.Builder()
+                .baseUrl("https://api.douban.com/v2/")
+                .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+                .addConverterFactory(GsonConverterFactory.create())
+                .client(okHttpClient)
+                .build();
+        DouBanApiservice douBanApiservice = retrofit.create(DouBanApiservice.class);
+       /* douBanApiservice.getMovies("top250")
+                .subscribeOn(Schedulers.io())
+                .subscribe(new Observer<MovieModel>() {
+                    @Override
+                    public void onSubscribe(@NonNull Disposable d) {
+
+                    }
+
+                    @Override
+                    public void onNext(@NonNull MovieModel movieModel) {
+                        System.out.println(movieModel.toString());
+                    }
+
+                    @Override
+                    public void onError(@NonNull Throwable e) {
+                    System.out.println(e.getMessage());
+                    }
+
+                    @Override
+                    public void onComplete() {
+                        System.out.println("complete");
+                    }
+                });*/
+        douBanApiservice.getMovies()
+                .map(new HttpReusltFunc<List<MovieModel.SubjectsBean>>())
+                .subscribe(new Observer<List<MovieModel.SubjectsBean>>() {
+                    private Disposable mDisposable;
+                    @Override
+                    public void onSubscribe(@NonNull Disposable d) {
+                        this.mDisposable=d;
+                    }
+
+                    @Override
+                    public void onNext(@NonNull List<MovieModel.SubjectsBean> subjectsBeen) {
+                     System.out.println(subjectsBeen.size());
+                    }
+
+                    @Override
+                    public void onError(@NonNull Throwable e) {
+                    }
+
+                    @Override
+                    public void onComplete() {
+
+                    }
+                });
+
+    }
+
+    interface DouBanApiservice{
+        @GET("movie/{arg}")
+        Observable<MovieModel> getMovies(@Path("arg") String arg);
+
+        @GET("movie/top250")
+        Observable<HttpResult<List<MovieModel.SubjectsBean>>> getMovies();
+    }
+
+    private abstract class BaseHttpResult<T>{
+
+    }
+    private class HttpResult<T>{
+        public int count;
+        public int start;
+        public int total;
+        public T subjects;
+    }
+
+    private class HttpReusltFunc<T> implements Function<HttpResult<T>,T>{
+        @Override
+        public T apply(@NonNull HttpResult<T> tHttpResult) throws Exception {
+            if(tHttpResult.subjects==null)
+                throw new RuntimeException("get nothing error!");
+            return tHttpResult.subjects;
+        }
+    }
+
 }
